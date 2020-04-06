5eae78ac6895af555b513f9b5d23f35bfa047e95
vaidehibagwe <58589944+vaidehibagwe@users.noreply.github.com>
2020-04-07 00:47:40 +0530

    Update evenno.py

--- evenno.py
+++ evenno.py
@@ -1,16 +1,11 @@
-# Even no. in list
 # Python program to print Even Numbers in a List 
   
 # list of numbers 
-list1 = [10, 21, 4, 45, 66, 93, 56, 45, 74, 86] 
-num = 0
+list1 = [10, 21, 4, 45, 66, 93] 
   
-# using while loop         
-while(num < len(list1)): 
+# iterating each number in list 
+for num in list1: 
       
     # checking condition 
     if num % 2 == 0: 
-       print(list1[num], end = " ") 
-      
-    # increment num   
-    num += 1
+       print(num, end = " ")
