fad95cc8515f9d8b4b1b5bdf3d943e8bbca48327
vaidehibagwe <58589944+vaidehibagwe@users.noreply.github.com>
2020-04-07 01:43:23 +0530

    Update even.py

--- even.py
+++ even.py
@@ -1,10 +1,1 @@
-# Python program to print Even Numbers in given range 
-  
-start, end = 4, 19
-  
-# iterating each number in list 
-for num in range(start, end + 1): 
-      
-    # checking condition 
-    if num % 2 == 0: 
-        print(num, end = " ")
+print("Goodbye, World!")
